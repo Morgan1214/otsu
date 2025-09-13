08379cc95f07d4379e3186fb1c5c91c82dcfbc85
Morgan Guo <sichengg@andrew.cmu.edu>
2025-09-13 18:12:11 -0400

    yes

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 #include "game.h"
 
 int main(int argc, char* argv[]) {
-    char beatmap_path[100], soundtrack_path[100];
-  if (argc != 2) {
-    printf("Usage: %s <Test directory>\n", argv[0]);
+  if (argc < 3) {
+    printf("Usage: %s <path/to/.otsu file> <path/to/.ogg file>\n", argv[0]);
     return 0;
   }
-  snprintf(beatmap_path, 100, "%s/beatmap.otsu", argv[1]);
-  snprintf(soundtrack_path, 100, "%s/audio.ogg", argv[1]);
-  return game_run(beatmap_path, soundtrack_path);
+  return game_run(argv[1], argv[2]);
 }
 
